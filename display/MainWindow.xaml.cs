6e0051166e60897ca2f2dfc9bb77c8a2bb6c372f
lexasss <oleg.spakov@tuni.fi>
2023-09-19 12:32:30 +0300

    refactored

--- display/MainWindow.xaml.cs
+++ display/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,54 +9,22 @@ using Websocket.Client;
 
 namespace PupilSizeDisplay;
 
-public enum Source { Diameter, Area }
-
-public class PupilUI
-{
-    public LiveData Graph => _graph;
-    public Border Bar => _bar;
-    public TextBlock Value => _value;
-    public Queue<Types.Pupil> Queue => _queue;
-    public PupilUI(LiveData graph, Border bar, TextBlock value)
-    {
-        _graph = graph;
-        _bar = bar;
-        _value = value;
-    }
-
-    // Internal
-
-    private readonly LiveData _graph;
-    private readonly Border _bar;
-    private readonly TextBlock _value;
-    private readonly Queue<Types.Pupil> _queue = new();
-}
-
 public partial class MainWindow : Window
 {
-    private readonly PupilUI[] _ui;
+    private readonly PupilProcessor _ui;
 
     private readonly JsonSerializerOptions _jsonSerializerOptions = new() { PropertyNameCaseInsensitive = true };
 
-    private const int MAX_QUEUE_SIZE = 5;       // samples to average
-    private const double DATA_SOURCE_FREQUENCY = 120; // Hz
-    private const double DATA_UPDATE_FREQUENCY = DATA_SOURCE_FREQUENCY / MAX_QUEUE_SIZE;  // Hz
-
-
     private WebsocketClient? _client;
 
-    private readonly double[] _maxPupilSize = {0, 0};
     private Source _source = Source.Diameter;
-    private int _pupilVisible = 0;
+    private int _pupilVisible = 0; // 0 (left) or 1 (right)
 
     public MainWindow()
     {
         InitializeComponent();
 
-        _ui = new[] {
-            new PupilUI(lvdChartLeft, brdSizeLeft, tblSizeLeft),
-            new PupilUI(lvdChartRight, brdSizeRight, tblSizeRight)
-        };
+        _ui = new PupilProcessor(lvdChart, brdSize, tblSize, brdMax);
 
         CreateWebSocketClient();
 
@@ -82,7 +48,8 @@ public partial class MainWindow : Window
         _client.Start();
     }
 
-    private void OnDisconnected(DisconnectionInfo info) => Dispatcher.Invoke(() => {
+    private void OnDisconnected(DisconnectionInfo info) => Dispatcher.Invoke(() =>
+    {
         tblConnection.Text = "disconnected";
         elpCorrectionSign.Fill = Brushes.Red;
 
@@ -91,12 +58,12 @@ public partial class MainWindow : Window
         DispatchOnce.Do(3, CreateWebSocketClient);
     });
 
-    private void OnConnected(ReconnectionInfo info) => Dispatcher.Invoke(() => {
+    private void OnConnected(ReconnectionInfo info) => Dispatcher.Invoke(() =>
+    {
         tblConnection.Text = "connected";
         elpCorrectionSign.Fill = Brushes.Green;
 
         System.Diagnostics.Debug.WriteLine("Connected");
-        //ResetGraphs();
     });
 
     private void OnMessage(ResponseMessage msg)
@@ -106,60 +73,15 @@ public partial class MainWindow : Window
         {
             try
             {
-                Dispatcher.Invoke(() => HandlePupil(pupil));
+                Dispatcher.Invoke(() =>
+                {
+                    if (_pupilVisible == (1 - pupil.Id))
+                        _ui.Add(pupil, _source);
+                });
             }
             catch (TaskCanceledException)
             { }
         }
-    }
-
-    private void ResetGraphs()
-    {
-        _ui?[_pupilVisible].Graph.Reset(DATA_UPDATE_FREQUENCY / DATA_SOURCE_FREQUENCY / LiveData.PixelsPerPoint, 0);
-    }
-
-    private void HandlePupil(Types.Pupil pupil)
-    {
-        var id = 1 - pupil.Id;
-
-        if (_pupilVisible != id)
-            return;
-
-        var size = _source switch
-        {
-            Source.Diameter => pupil.Diameter3d,
-            Source.Area => pupil.Diameter,
-            _ => 0
-        };
-
-        if (size < 0)
-            return;
-
-        _ui[id].Value.Text = size.ToString("F2");
-        _ui[id].Queue.Enqueue(pupil);
-
-        UpdateGraphAndBar(id);
-    }
-
-    private void UpdateGraphAndBar(int id)
-    {
-        if (_ui[id].Queue.Count != MAX_QUEUE_SIZE)
-            return;
-
-        var mean = _ui[id].Queue.Average(pupil => _source switch
-        {
-            Source.Diameter => pupil.Diameter3d,
-            Source.Area => pupil.Diameter,
-            _ => 0
-        });
-        _ui[id].Graph.Add(Utils.Timestamp.Sec, mean);
-
-        if (_maxPupilSize[id] < mean)
-            _maxPupilSize[id] = mean;
-
-        _ui[id].Bar.Height = (_ui[id].Bar.Parent as Grid)!.ActualHeight * mean / _maxPupilSize[id];
-
-        _ui[id].Queue.Clear();
     }
 
     private void App_Exit(object sender, ExitEventArgs e)
@@ -171,19 +93,19 @@ public partial class MainWindow : Window
 
     private void Window_Loaded(object sender, RoutedEventArgs e)
     {
-        ResetGraphs();
+        _ui?.Clear();
         lsvSource.Focus();
     }
 
     private void Source_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
         _source = (Source)Enum.Parse(typeof(Source), lsvSource.SelectedItem?.ToString() ?? "");
-        ResetGraphs();
+        _ui?.Clear();
     }
 
     private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
         _pupilVisible = tbcTabs.SelectedIndex;
-        ResetGraphs();
+        _ui?.Clear();
     }
 }
---
+++ display/PupilProcessor.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace PupilSizeDisplay;
+
+public enum Source { Diameter, Area }
+
+public class PupilProcessor
+{
+    public TextBlock Value => _value;
+    public PupilProcessor(LiveData graph, Border bar, TextBlock value, Border max)
+    {
+        _graph = graph;
+        _bar = bar;
+        _value = value;
+        _max = max;
+    }
+
+    public void Clear()
+    {
+        _maxPupilSize = 0;
+        _graph.Reset(DATA_UPDATE_FREQUENCY / DATA_SOURCE_FREQUENCY / LiveData.PixelsPerPoint, 0);
+    }
+
+    public void Add(Types.Pupil pupil, Source source)
+    {
+        var size = source switch
+        {
+            Source.Diameter => pupil.Diameter3d,
+            Source.Area => pupil.Diameter,
+            _ => 0
+        };
+
+        if (size < 0)
+            return;
+
+        _value.Text = size.ToString("F2");
+        _queue.Enqueue(pupil);
+
+        if (_queue.Count == MAX_QUEUE_SIZE)
+        {
+            Update(source);
+        }
+    }
+
+    // Internal
+
+    private const int MAX_QUEUE_SIZE = 5;       // samples to average
+    private const double DATA_SOURCE_FREQUENCY = 120; // Hz
+    private const double DATA_UPDATE_FREQUENCY = DATA_SOURCE_FREQUENCY / MAX_QUEUE_SIZE;  // Hz
+
+    private readonly LiveData _graph;
+    private readonly Border _bar;
+    private readonly TextBlock _value;
+    private readonly Border _max;
+    private readonly Queue<Types.Pupil> _queue = new();
+
+    private double _maxPupilSize = 0;
+
+    private void Update(Source source)
+    {
+        var mean = _queue.Average(pupil => source switch
+        {
+            Source.Diameter => pupil.Diameter3d,
+            Source.Area => pupil.Diameter,
+            _ => 0
+        });
+
+        if (_maxPupilSize < mean)
+            _maxPupilSize = mean;
+
+        SetValue(mean, _maxPupilSize);
+
+        _queue.Clear();
+    }
+
+    private void SetValue(double mean, double max)
+    {
+        _graph.Add(Utils.Timestamp.Sec, mean);
+
+        var barHeight = (_bar.Parent as Grid)!.ActualHeight * mean / max;
+        _bar.Height = barHeight;
+
+        var bottomMax = Math.Max(_max.Margin.Bottom - 1, barHeight);
+        _max.Margin = new Thickness(0, 0, 0, bottomMax);
+    }
+}
