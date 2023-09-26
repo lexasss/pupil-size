476348f2692782c2e806b134f179ebfd221a0fbd
lexasss <oleg.spakov@tuni.fi>
2023-09-26 11:24:33 +0300

    using TCP for SmartEye

--- display/Trackers/SmartEye.cs
+++ display/Trackers/SmartEye.cs
@@ -10,42 +10,76 @@ namespace PupilSizeDisplay.Trackers
         public event EventHandler<DeviceInfo>? DeviceStateChanged;
         public event EventHandler<double>? Sample;
 
-        public SmartEye(string ip)
+        public SmartEye(string ip, bool useTcp = true)
         {
-            _dataSource.Data += DataSource_Data;
-            _dataSource.Closed += DataSource_Closed;
+            _useTcp = useTcp;
 
-            _parser.Sample += Parser_Sample;
+            if (_useTcp)
+            {
+                _tcpClient = new SEClient.Tcp.Client();
+                _tcpClient.Sample += (s, e) =>
+                {
+                    if (Eye == Eye.Left && e.LeftPupilDiameter is not null)
+                    {
+                        Sample?.Invoke(this, (double)e.LeftPupilDiameter * 1000);
+                    }
+                    else if (Eye == Eye.Right && e.RightPupilDiameter is not null)
+                    {
+                        Sample?.Invoke(this, (double)e.RightPupilDiameter * 1000);
+                    }
+                };
+                _tcpClient.Connected += (s, e) => DeviceStateChanged?.Invoke(this, new DeviceInfo() { Name = "Smart Eye Pro", State = DeviceState.Connected });
+                _tcpClient.Disconnected += (s, e) => DeviceStateChanged?.Invoke(this, new DeviceInfo() { State = DeviceState.None });
 
-            DispatchOnce.Do(0.1, () => {
-                DeviceStateChanged?.Invoke(this, new DeviceInfo() { Name = "Smart Eye Pro", State = DeviceState.Connected });
-                _parser.Reset();
-                _dataSource.Start(ip, 5002.ToString());
-            });
+                DispatchOnce.Do(0.1, () =>
+                {
+                    _tcpClient.Connect(ip, 5002);
+                });
+            }
+            else
+            {
+                _cmdDataSource = new();
+                _cmdDataSource.Data += CmdDataSource_Data;
+                _cmdDataSource.Closed += CmdDataSource_Closed;
+
+                _cmdParser = new();
+                _cmdParser.Sample += CmdParser_Sample;
+
+                DispatchOnce.Do(0.1, () =>
+                {
+                    DeviceStateChanged?.Invoke(this, new DeviceInfo() { Name = "Smart Eye Pro", State = DeviceState.Connected });
+                    _cmdParser.Reset();
+                    _cmdDataSource.Start(ip, 5002.ToString());
+                });
+            }
         }
 
         public void Dispose()
         {
-            _dataSource.Stop();
+            _tcpClient?.Dispose();
+            _cmdDataSource?.Stop();
+
             GC.SuppressFinalize(this);
         }
 
         // Internal
 
-        readonly SEClient.DataSource _dataSource = new();
-        readonly SEClient.Parser _parser = new();
+        readonly bool _useTcp;
+        readonly SEClient.Tcp.Client? _tcpClient;
+        readonly SEClient.Cmd.DataSource? _cmdDataSource;
+        readonly SEClient.Cmd.Parser? _cmdParser;
 
-        private void DataSource_Closed(object? sender, EventArgs e)
+        private void CmdDataSource_Closed(object? sender, EventArgs e)
         {
             DeviceStateChanged?.Invoke(this, new DeviceInfo() { State = DeviceState.None });
         }
 
-        private void DataSource_Data(object? sender, string e)
+        private void CmdDataSource_Data(object? sender, string e)
         {
-            _parser.Feed(e);
+            _cmdParser?.Feed(e);
         }
 
-        private void Parser_Sample(object? sender, SEClient.Sample e)
+        private void CmdParser_Sample(object? sender, SEClient.Cmd.Sample e)
         {
             Sample?.Invoke(this, e.EyeFeature[(int)Eye].Size.Diameter * 1000);
         }
